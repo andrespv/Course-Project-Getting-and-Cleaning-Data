158a0ffe46dca0a3f8f0400fb7174511a8e5f437
andrespv <prep.computo@gmail.com>
2015-12-17 16:02:26 -0430

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,13 +5,12 @@ library(plyr)
 filename <- "getdata-projectfiles-UCI HAR Dataset.zip"
 
 ## Download and unzip the dataset:
-if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileURL, filename, method="auto")
-}  
-if (!file.exists("UCI HAR Dataset")) { 
-  unzip(filename) 
-}
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, filename, method="auto")
+
+unzip(filename) 
+
 
 ### Merges the training and the test sets to create one data set
 
@@ -23,13 +22,8 @@ x_test <- read.table("test/X_test.txt")
 y_test <- read.table("test/y_test.txt")
 subject_test <- read.table("test/subject_test.txt")
 
-# create 'x' data set
 x_data <- rbind(x_train, x_test)
-
-# create 'y' data set
 y_data <- rbind(y_train, y_test)
-
-# create 'subject' data set
 subject_data <- rbind(subject_train, subject_test)
 
 ### Extracts only the measurements on the mean and standard deviation for each measurement
@@ -40,16 +34,16 @@ features[,2] <- as.character(features[,2])
 
 # Extract only the data on mean and standard deviation
 featuresnum <- grep(".*mean.*|.*std.*", features[,2])
-featuresnum.names <- features[featuresnum,2]
-featuresnum.names = gsub('-mean', 'Mean', featuresnum.names)
-featuresnum.names = gsub('-std', 'Std', featuresnum.names)
-featuresnum.names <- gsub('[-()]', '', featuresnum.names)
+featuresnum_names <- features[featuresnum,2]
+featuresnum_names = gsub('-mean', 'Mean', featuresnum_names)
+featuresnum_names = gsub('-std', 'Std', featuresnum_names)
+featuresnum_names <- gsub('[-()]', '', featuresnum_names)
 
-# subset the desired columns
+# subset the wanted colums
 x_data <- x_data[, featuresnum]
 
-# New column names
-names(x_data) <- featuresnum.names
+# putting the names of the features wanted
+names(x_data) <- featuresnum_names
 
 
 ### Uses descriptive activity names to name the activities in the data set
@@ -58,7 +52,7 @@ names(x_data) <- featuresnum.names
 activities <- read.table("activity_labels.txt")
 activities[,2] <- as.character(activities[,2])
 
-# update values with correct activity names
+# putting the names of every activities
 y_data[, 1] <- activities[y_data[, 1], 2]
 
 # New column name
@@ -72,13 +66,13 @@ names(y_data) <- "activity"
 names(subject_data) <- "subject"
 
 # Unify all the data
-all_data <- cbind(x_data, y_data, subject_data)
+alldata <- cbind(x_data, y_data, subject_data)
 
 
 ### From the data set in step 4, creates a second, independent tidy data set with the average 
 ### of each variable for each activity and each subject.
 
-tidydata <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+tidydata <- ddply(alldata, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
 write.table(tidydata, "tidydata.txt", row.name=FALSE)
 
